942b452031e34679b010107579300d90d213a78a
jakobpotosme <56290282+jakobpotosme@users.noreply.github.com>
2021-11-10 22:18:16 -0800

    Added create user page and login steps for user/teacher

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, url_for
+from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, BaseView, expose
+from flask_login import current_user, login_user, LoginManager, UserMixin
+from werkzeug.utils import redirect
 # from database import db, Students, Teachers, Admin
 
 app = Flask(__name__)
@@ -8,8 +11,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 db = SQLAlchemy(app)
 admin = Admin(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+app.secret_key = 'keep it secret, keep it safe'
 
-class Students(db.Model):
+
+class Students(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=False, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -17,8 +25,26 @@ class Students(db.Model):
     studentEnrollment = db.relationship(
         'Enrollment', backref='Students', lazy=True)
 
+    # def is_authenticated(self, username, password):
+    #     if self.username == username and self.password == password:
+    #         return True
+    #     else:
+    #         return False
 
-class Teachers(db.Model):
+    # def is_anonymous(self, username):
+    #     if self.username != username:
+    #         return True
+    #     else:
+    #         return False
+
+    # def get_id(self):
+    #     return self.id
+
+    # def check_password(self, password):
+    #     return self.password == password
+
+
+class Teachers(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=False, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -71,11 +97,78 @@ class Enrollment(db.Model):
 # @app.route('/teacherview')
 # admin page done with flask-admin
 
+# Flask-Login lecture 18 page 15
+
+@login_manager.user_loader
+def load_student(user_id):
+    return Students.get_id(user_id)
+
+
+@login_manager.user_loader
+def load_teacher(user_id):
+    return Teachers.get_id(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = ''
+        password = ''
+        username += request.form['username']
+        password += request.form['password']
+        permission = request.form['permType']
+
+        if(permission == 'student'):
+            user = Students.query.filter_by(username=username).first()
+            if(user is None):
+
+                return 'no account found'
+            else:
+                # login_user(user)
+                # return redirect(url_for('student'))
+                return render_template('student.html', student=user)
+        elif(permission == 'teacher'):
+            user = Teachers.query.filter_by(username=username).first()
+            if(user is None):
+
+                return 'no account found'
+            else:
+                # login_user(user)
+                print('Logged in Successfully')
+                return render_template('teacher.html', teacher=user)
+
+    return render_template('login.html')
+
+
+@app.route('/student', methods=['GET', 'POST'])
+def student():
+    return render_template('student.html')
+
 
 class CreatePage(BaseView):
-    @expose('/')
-    def index(self):
-        return self.render('admin.html')
+    @expose('/', methods=['GET', 'POST'])
+    def createUser(self):
+
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+            username = ''
+            password = ''
+            username += request.form['username']
+            password += request.form['password']
+            name = request.form['name']
+            permission = request.form['permType']
+
+            if(permission == 'student'):
+                newEntry = Students(username=username,
+                                    password=password, name=name)
+                db.session.add(newEntry)
+                db.session.commit()
+            elif(permission == 'teacher'):
+                newEntry = Teachers(username=username,
+                                    password=password, name=name)
+                db.session.add(newEntry)
+                db.session.commit()
+
+        return self.render('create.html')
 
 
 class ReadUsers(BaseView):
